f59b16a0eeda57f38f1ad37548581340e5972afb
Domidhbw <dom.neubauer.23@lehre.mosbach.dhbw.de>
2024-03-01 11:14:44 +0100

    test

--- AGameHackersBlog/index.php
+++ AGameHackersBlog/index.php
@@ -4,7 +4,7 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>A Game Hackers Blog</title>
-      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="css/style.css">
       <link rel="stylesheet" href="https://use.typekit.net/doe1yvq.css">
    </head>
    <body>
