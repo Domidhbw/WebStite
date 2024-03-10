e55b782ce398cc952baa7114fa82bd4aa94ea8ac
Tim Leute <tim.leute@wuerth-industrie.com>
2024-03-10 18:05:33 +0100

    ordnerstruktur

---
+++ inf23-01-GameHackersBlog/php/impressum.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../favicon.ico">
+    <link rel="stylesheet" href="../css/templates.css">
+    <link rel="stylesheet" href="../css/impressum.css">
+    <title>Impressum</title>
+</head>
+<body>
+    <?php include 'templates/header.php';  ?>
+    <?php include 'templates/navPages.php';  ?>
+    <h1>Impressum</h1>
+    <p>Firmenname: Beispiel GmbH</p>
+    <p>Adresse: Musterstraße 123, 12345 Musterstadt</p>
+    <p>E-Mail: dominik.neubauer@wuerth-industrie.com</p>
+    <p>Telefon: +49 123 456789</p>
+    <p>Vertreten durch: Max Mustermann</p>
+    <p>Handelsregisternummer: HRB 12345</p>
+    <p>Registergericht: Amtsgericht Musterstadt</p>
+    <p>Umsatzsteuer-Identifikationsnummer gemäß § 27a UStG: DE 123456789</p>
+    <p>Zuständige Aufsichtsbehörde: Industrie- und Handelskammer Musterstadt</p>
+    <p>Berufsbezeichnung: Informatiker</p>
+    <p>Haftungsausschluss: Für die Inhalte externer Links übernehmen wir keine Haftung.</p>
+    <p>Urheberrecht: Die Inhalte dieser Website unterliegen dem deutschen Urheberrecht.</p>
+    <p>Datenschutz: Informationen zum Datenschutz finden Sie in unserer <a href="datenschutz.php">Datenschutzerklärung</a>.</p>
+    <p>Streitschlichtung: Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit. Diese finden Sie unter <a href="http://ec.europa.eu/consumers/odr">http://ec.europa.eu/consumers/odr</a>.</p>
+</body>
+<?php include 'templates/footerPages.php'; ?>
+
+</html>
