5ead07cbe67aa109bb15fb8f4bb1d5d0eacd0b49
Domidhbw <dom.neubauer.23@lehre.mosbach.dhbw.de>
2024-03-10 15:08:44 +0100

    Revert "fix ordnerstruktur"
    This reverts commit 14002c7efc01b78b9fcf547599764f836e215dec.

--- php/templates/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-  <nav class="bottomnav">
-    <a href="php/datenschutz.php">Datenschutz</a>
-    <a href="php/impressum.php">Impressum</a>
-  </nav>
-</footer>
---
+++ test/php/templates/footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <nav class="bottomnav">
+    <a href="#">Datenschutz</a>
+    <a href="#">Impressum</a>
+  </nav>
+</footer>
