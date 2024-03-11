173b9f185577b229aed33cc149610486d6dee826
Domidhbw <dom.neubauer.23@lehre.mosbach.dhbw.de>
2024-03-11 17:57:51 +0100

    added empty about us

---
+++ inf23-01-GameHackersBlog/php/aboutus.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="../favicon.ico">
+  <link rel="stylesheet" href="../css/templates.css">
+  <link rel="stylesheet" href="../css/aboutus.css">
+  <title>About Us</title>
+</head>
